88665466f7d8d49acc4e1cd9b2570baf344e1bd9
henrik <>
2025-04-20 20:04:35 +0200

    build.sh: Support parallel build

--- README.md
+++ README.md
@@ -2,18 +2,18 @@
 ## Simple Build
 `mkdir build`  
 `cd build`  
-`CFLAGS=-Os LDFLAGS=-s ../src/build.sh`  
+`PARALLEL=$(nproc) CFLAGS=-Os LDFLAGS=-s ../src/build.sh`  
 `./sdl3-opengl`  
 
 ## Cross Build
 `. setup_toolchain.sh`  
 `mkdir build-cross`  
 `cd build-cross`  
-`CFLAGS=-Os LDFLAGS=-s ../src/build.sh`  
+`PARALLEL=$(nproc) CFLAGS=-Os LDFLAGS=-s ../src/build.sh`  
 
 ## Configure a Makefile
 `. setup_toolchain.sh`  
 `mkdir build-dev`  
 `cd build-dev`  
-`CONFIGURE=1 CFLAGS=-g ../src/build.sh`  
+`PARALLEL=$(nproc) CONFIGURE=1 CFLAGS="-g -Wall -Wextra -Wconversion -Wno-sign-conversion -fsanitize=undefined" LDFLAGS="-fsanitize=undefined" ../src/build.sh`  
 `make`  
--- src/build.sh
+++ src/build.sh
@@ -19,9 +19,29 @@ escape() {
     printf \'
 }
 
+compile() {
+    cmd="$CC -c $DEP_CFLAGS $GL_CFLAGS $SDL3_CFLAGS $CFLAGS $(escape "$SCRIPT_DIR/$1.c")"
+    printf "%s\n" "$cmd"
+    eval "$cmd"
+    if test "$CONFIGURE"; then
+        dep_cmd="mv $source.d $source.o.d"
+        eval "$dep_cmd"
+        printf "%s.o:\n\t%s\n\t@%s\ninclude %s.o.d\n" "$1" "$cmd" "$dep_cmd" "$1" >> Makefile
+    fi
+}
+
+wait_compile() {
+    set -- $pids
+    wait "$1"
+    shift
+    pids="$@"
+    started=$(($started-1))
+}
+
 CC="${CC:-cc}"
 PKG_CONFIG="${PKG_CONFIG:-pkg-config}"
 
+PARALLEL="${PARALLEL:-1}"
 DEP_CFLAGS="${DEP_CFLAGS-"-MD -MP"}"
 if test "$CONFIGURE"; then
     printf ".POSIX:\nsdl3-opengl:\n" > Makefile
@@ -35,17 +55,19 @@ SDL3_LIBS="${SDL3_LIBS-"$("$PKG_CONFIG" --libs sdl3)"}"
 
 c_sources="sdl3-opengl"
 objects=
+pids=
+started=0
 for source in $c_sources
 do
-    cmd="$CC -c $DEP_CFLAGS $GL_CFLAGS $SDL3_CFLAGS $CFLAGS $(escape "$SCRIPT_DIR/$source.c")"
-    printf "%s\n" "$cmd"
-    eval "$cmd"
-    if test "$CONFIGURE"; then
-        dep_cmd="mv $source.d $source.o.d"
-        eval "$dep_cmd"
-        printf "%s.o:\n\t%s\n\t@%s\ninclude %s.o.d\n" "$source" "$cmd" "$dep_cmd" "$source" >> Makefile
-    fi
-    objects="$source.o $objects"
+    compile "$source" &
+    pids="$pids $!"
+    objects="$objects $source.o"
+    started=$(($started+1))
+    test "$started" -ge "$PARALLEL" && wait_compile
+done
+for pid in $pids
+do
+    wait "$pid"
 done
 
 cmd="$CC -o sdl3-opengl $LDFLAGS $objects $SDL3_LIBS $LIBS"
